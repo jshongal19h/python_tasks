9e11e33183a43f1c1b7ffc3c0c738b2fa789bfea
jshongal19h <jagdishshongal@gmail.com>
2025-12-07 12:46:03 +0530

    Commit Message

---
+++ ASSIGMNET1_Task2.py
@@ -0,0 +1,13 @@
+# Task 2: Create a Personalized Greeting
+# Problem Statement: Write a Python program that:
+# 1.  Takes a user's first name and last name as input.
+# 2.  Concatenates the first name and last name into a full name.
+# 3.  Prints a personalized greeting message using the full name.
+# Expected Output:
+# The program should output a greeting like:
+
+First_name = input("Enter your First name:")
+Secondt_name = input("Enter your Second name:")
+
+Greeting = First_name + ' ' + Secondt_name
+print("Hello", Greeting +"!" , "How are you today? How can i help you?")
