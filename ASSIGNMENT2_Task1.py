5019df0634f2debe751ca8e5208179979021f2cd
jshongal19h <jagdishshongal@gmail.com>
2025-12-09 16:26:28 +0530

    Commit Message

---
+++ ASSIGNMENT2_Task1.py
@@ -0,0 +1,16 @@
+# ASSIGNMENT 2:
+# Module 3: Control Structures in Python
+ 
+# Task 1: Check if a Number is Even or Odd
+# Problem Statement:  Write a Python program that:
+# 1. 	Takes an integer input from the user.
+# 2. 	Checks whether the number is even or odd using an if-else statement.
+# 3. 	Displays the result accordingly.
+# Expected Output:
+# The program should return an output like:
+
+Input = int(input("Enter an integer number: "))
+if Input % 2 == 0:
+    print("The number", Input, "is Even.")
+else:
+    print("The number", Input, "is Odd.")
